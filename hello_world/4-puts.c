963d1ebb5b9323f207ee8853882dfdcc3cb58dc9
Your Name <bezom@hotmail.fr>
2025-02-16 14:32:45 +0100

    a

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
+
 int main() {
-    puts("Programming is like building a multilingual puzzle,");
-    puts("");
+    puts("Programming is like building multilingual puzzle");
     return 0;
 }
