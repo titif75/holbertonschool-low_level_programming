2052c86aca65ffb5c965cabef7b0c3cf187039b0
Your Name <bezom@hotmail.fr>
2025-02-16 14:01:40 +0100

    a

---
+++ hello_world/cours1.c
@@ -0,0 +1,12 @@
+int main(){
+	//type int: concerne les nombres entiers sans virgule  1 2 3 4 5 6
+	int nombre1 = 1;
+	int nombre2 = 10;
+	
+	// type float: concerne les nombres décimaux
+	float pi = 3.14; 
+
+
+
+	return nombre1 + nombre2;
+}
