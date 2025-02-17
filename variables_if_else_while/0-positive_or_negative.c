a11a32b83a7a7861aed5d5340719f14a5e24b361
Your Name <bezom@hotmail.fr>
2025-02-17 16:37:58 +0100

    a

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -14,10 +14,13 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
+printf("%d", n);
 if (n> 0) {
 printf(" is positive\n");
-} else if (n < 0) {
-} else {
+}
+else if (n < 0) {
+printf(" is negative\n");
+}else {
 printf(" is zero\n");
 }
 	return (0);
