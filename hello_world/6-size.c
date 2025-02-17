4173aee10ec80e3a4067158b480f76d4a11b6520
Your Name <bezom@hotmail.fr>
2025-02-17 12:29:35 +0100

    a

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -11,6 +11,5 @@ int main(void)
     printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
     printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
     printf("Size of a float: %zu byte(s)\n", sizeof(float));
-    printf("Size of a double: %zu byte(s)\n", sizeof(double));
     return (0);
 }
