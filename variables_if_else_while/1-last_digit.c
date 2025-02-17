d8cd23b8704078e9c21f34a4a902e44bf19928cf
Your Name <bezom@hotmail.fr>
2025-02-17 22:15:08 +0100

    a

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-int main(void) {
-srand(time(NULL));
-int n = rand() % 100; // Generate a random number between 0 and 99
-printf("The string Last digit of %d is, ", n);
-int last_digit = n % 10;
-if (last_digit > 5) {
-printf("and is greater than 5\n");
-}
-else if (last_digit == 0) {
-printf("and is 0\n");
-} else
-{
-printf("and is less than 6 and not 0\n");
-}
-return 0;
+
+int main() {
+    srand(time(NULL));
+    int n; // Déclaration de n
+
+    n = rand() % 100; // Initialisation de n
+
+    printf("The string Last digit of %d is, ", n);
+
+    int last_digit = n % 10;
+
+    if (last_digit > 5) {
+        printf("and is greater than 5\n");
+    } else if (last_digit == 0) {
+        printf("and is 0\n");
+    } else {
+        printf("and is less than 6 and not 0\n");
+    }
+
+    return 0;
 }
