f3867a796b6400e528eeaf65208c517d6b534b20
Your Name <bezom@hotmail.fr>
2025-02-17 14:57:27 +0100

    a

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    // Initialiser la graine aléatoire
+    srand(time(NULL));
+
+    // Générer un nombre aléatoire
+    int n = rand() % 200 - 100; // Génère des nombres entre -100 et 99
+
+    // Afficher si le nombre est positif, négatif ou zéro
+    printf("%d", n);
+    
+    if (n > 0) {
+        printf(" est positif\n");
+    } else if (n < 0) {
+        printf(" est négatif\n");
+    } else {
+        printf(" est zéro\n");
+    }
+
+    return 0;
+}
