761db68b8edc190ddfd80d020011e60a3eba60f1
Your Name <bezom@hotmail.fr>
2025-02-18 00:31:53 +0100

    a

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -2,20 +2,17 @@
 #include <stdlib.h>
 #include <time.h>
 int main(void) {
-int n; /* Déclaration de n */
-int last_digit; /* Déclaration de last_digit */
-srand(time(NULL));
-n = rand() % 100; /* Initialisation de n */
-printf("Last digit of %d is ", n);
-last_digit = n % 10; /* Calcul de la dernière chiffre */
-printf("%d ", last_digit); /* Afficher la dernière chiffre avec un espace */
-if (last_digit > 5) {
-printf("and is greater than 5\n");
-} else if (last_digit == 0) {
-printf("and is 0\n");
-} else
-{
-printf("and is less than 6 and not 0\n");
-}
-return 0;
+    srand(time(NULL));
+    int n = rand() % 100; /* Générer un nombre aléatoire entre 0 et 99 */
+    printf("The string Last digit of %d is, ", n);
+    int last_digit; /* Variable pour stocker le dernier chiffre de n */
+    last_digit = n % 10; /* Calculer la dernière chiffre */
+    if (last_digit > 5) {
+        printf("and is greater than 5\n");
+    } else if (last_digit == 0) {
+        printf("and is 0\n");
+    } else {
+        printf("and is less than 6 and not 0\n");
+    }
+    return 0;
 }
