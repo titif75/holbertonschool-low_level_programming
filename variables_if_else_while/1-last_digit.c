cb4896f59e613fa2a7bb4061bbd98489000e126f
Your Name <bezom@hotmail.fr>
2025-02-18 07:44:11 +0100

    a

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,21 +1,23 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
  * main - Entry point of the program
  * Description: This function generates a random number and prints its last digit with the appropriate message.
  * Return: Always 0 (Success)
  */
 
-
-int main(void) -
+int main(void)
 {
-	int  n;
+    int n; // Déclaration de la variable
 
-    srand(time(0));
-    n = rand() - RAND_MAX / 2 
-    int dernier_chiffre = n % 10;
+    srand(time(0)); // Initialiser le générateur de nombres aléatoires
+    n = rand() - RAND_MAX / 2; // Générer un nombre aléatoire
 
+    int dernier_chiffre = n % 10; // Extraire le dernier chiffre
+
+    // Affichage du dernier chiffre et du message approprié
     printf("Last digit of %d is %d and ", n, dernier_chiffre);
 
     if (dernier_chiffre > 5)
@@ -30,6 +32,6 @@ int main(void) -
     {
         printf("is less than 6 and not 0\n");
     }
-	
-	return (0);
+
+    return (0);
 }
