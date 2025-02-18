29a4b5b22bfcae6d4a168f535f91184a6c12be2e
Your Name <bezom@hotmail.fr>
2025-02-18 15:24:13 +0100

    a

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,14 @@
-#ifndef MAIN_H
-#define MAIN_H
-void _putchar(char c);
-#endif /* MAIN_H */
+#include "main.h"
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return 0;
+}
