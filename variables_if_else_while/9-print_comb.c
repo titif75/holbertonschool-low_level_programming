ebb42de58ec6172d045b56bacdcf32b95d4c3a9d
Your Name <bezom@hotmail.fr>
2025-02-18 14:15:30 +0100

    a

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/**
+* main - Programme qui affiche tous les chiffres de 0 à 9,
+*        séparés par une virgule et un espace.
+*
+* Return: Toujours 0 (EXIT_SUCCESS)
+*/
 int main(void)
 {
 int i;
