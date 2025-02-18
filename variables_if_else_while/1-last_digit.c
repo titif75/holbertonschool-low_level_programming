2b842bc508e0e0e7dc4c2980ac07e22b331606f0
Your Name <bezom@hotmail.fr>
2025-02-18 07:53:24 +0100

    a

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -3,35 +3,29 @@
 #include <stdio.h>
 
 /**
- * main - Entry point of the program
- * Description: This function generates a random number and prints its last digit with the appropriate message.
- * Return: Always 0 (Success)
- */
+* main - Entry point of the program
+* Description: This function generates a random number and prints its last digit with the appropriate message.
+* Return: Always 0 (Success)
+*/
 
 int main(void)
 {
-    int n; // Déclaration de la variable
-
-    srand(time(0)); // Initialiser le générateur de nombres aléatoires
-    n = rand() - RAND_MAX / 2; // Générer un nombre aléatoire
-
-    int dernier_chiffre = n % 10; // Extraire le dernier chiffre
-
-    // Affichage du dernier chiffre et du message approprié
-    printf("Last digit of %d is %d and ", n, dernier_chiffre);
-
-    if (dernier_chiffre > 5)
-    {
-        printf("is greater than 5\n");
-    }
-    else if (dernier_chiffre == 0)
-    {
-        printf("is 0\n");
-    }
-    else
-    {
-        printf("is less than 6 and not 0\n");
-    }
-
-    return (0);
+int n;
+int dernier_chiffre = n % 10;
+srand(time(0)); // Initialiser le générateur de nombres aléatoires
+n = rand() - RAND_MAX / 2;
+printf("Last digit of %d is %d and ", n, dernier_chiffre);
+if (dernier_chiffre > 5)
+{
+printf("is greater than 5\n");
+}
+else if (dernier_chiffre == 0)
+{
+printf("is 0\n");
+}
+else
+{
+printf("is less than 6 and not 0\n");
+}
+return (0);
 }
