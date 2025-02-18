02c801163654dc2c6ba0b7d45c53c0370b647763
Your Name <bezom@hotmail.fr>
2025-02-18 13:59:42 +0100

    a

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -2,13 +2,15 @@
 int main(void)
 {
 int i;
-for (i = 0; i < 10; i++) {
+for (i = 0; i < 10; i++)
+{
 putchar(i + '0');
-if (i < 9) {
+if (i < 9) 
+{
 putchar(' , ');
-putchar('  '); 
+putchar('  ');
 }
 }
 putchar('\n');
-return 0;
+return (0);
 }
