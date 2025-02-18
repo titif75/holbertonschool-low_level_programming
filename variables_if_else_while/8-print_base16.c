9f44899687ed873f2c20a99513b0f40c1b290ebb
Your Name <bezom@hotmail.fr>
2025-02-18 11:36:51 +0100

    a

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 /**
-* main - Prints lowercase alphabet in reverse
+* main - Prints all hexadecimal numbers in lowercase
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-char letter = 'z';
-while (letter >= 'a')
-putchar(letter--);
+char digit = '0';
+while (digit <= '9')
+putchar(digit++);
+digit = 'a';
+while (digit <= 'f')
+putchar(digit++);
 putchar('\n');
 return (0);
 }
