2cf7771ff17c54db201fb23cef66a920981c978b
Your Name <bezom@hotmail.fr>
2025-02-18 12:18:11 +0100

    a

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 int main(void)
 {
-for (int i = 0; i < 10; i++)
-{
+int i;
+for (i = 0; i < 10; i++) {
 putchar(i + '0');
-if (i != 9)
-{
-putchar(',');  // Print the comma
-putchar(' ');  // Print the space
-}
+if (i != 9) {
+putchar(',');
+putchar(' '); }
 }
 putchar('\n');
 return 0;
