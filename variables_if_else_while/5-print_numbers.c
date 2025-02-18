c93a5d9704a134f3138e66f108dc70a00ec0690f
Your Name <bezom@hotmail.fr>
2025-02-18 11:22:39 +0100

    a

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Prints single digit numbers from 0 to 9
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int n = 0;
+while (n < 10)
+{
+printf("%d", n);
+n++;
+}
+printf("\n");
+return (0);
+}
