8962aaccee3c2f81693f980c78030286b13012a4
Your Name <bezom@hotmail.fr>
2025-02-19 15:40:28 +0100

    a

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,24 +1,20 @@
 #include "main.h"
 #include <unistd.h>
-
 void print_alphabet(void)
 {
-    char letter = 'a';
-
-    while (letter <= 'z')
-    {
-        write(1, &letter, 1);
-        letter++;
-    }
-    write(1, "\n", 1);
+char letter = 'a';
+while (letter <= 'z')
+{
+write(1, &letter, 1);
+letter++;
 }
-
+write(1, "\n", 1);
+}
 void print_alphabet_x10(void)
 {
-    int i;
-
-    for (i = 0; i < 10; i++)
-    {
-        print_alphabet();
-    }
+int i;
+for (i = 0; i < 10; i++)
+{
+print_alphabet();
+}
 }
