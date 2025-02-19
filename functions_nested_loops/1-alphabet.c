3f6c0c4743257dc4cf9a4c4522bc7cd5f3255616
Your Name <bezom@hotmail.fr>
2025-02-19 12:33:01 +0100

    a

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,17 +1,36 @@
-#include <stdio.h>
-#include <main.h>
+/* _putchar.c */
 #include <unistd.h>
+
 void _putchar(char c)
 {
-write(1, &c, 1);
+    write(1, &c, 1);
 }
+/* main.h */
+#ifndef MAIN_H
+#define MAIN_H
+
+void print_alphabet(void);
+
+#endif /* MAIN_H */
+/* main.c */
+#include "main.h"
+
+int main(void)
+{
+    print_alphabet();
+    return 0;
+}
+/* 1-alphabet.c */
+#include "main.h"
+
 void print_alphabet(void)
 {
-char letter = 'a';
-while (letter <= 'z')
-{
-_putchar(letter);
-letter++;
-}
-_putchar('\n');
+    char letter = 'a';
+
+    while (letter <= 'z')
+    {
+        _putchar(letter);
+        letter++;
+    }
+    _putchar('\n');
 }
