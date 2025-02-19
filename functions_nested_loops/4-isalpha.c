4cce293fbd6a6b31beb613e930bc82b9fc3d5d81
Your Name <bezom@hotmail.fr>
2025-02-19 19:33:24 +0100

    a

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - écrire une fonction qui détecte la lettre  en majuscule ou minuscule
+*/
+Return : 0
+int main(void){
+int _isalpha(int c){
+if(( c>="A" && c<="Z") || ( c>="a" && c<="z" )){
+return 1;
+}
+return 0;
+} 
+
