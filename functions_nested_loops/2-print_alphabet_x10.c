24c633703b53bc136cffd3194ee2afe96bf21093
Your Name <bezom@hotmail.fr>
2025-02-19 15:44:59 +0100

    a

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,11 @@
 #include "main.h"
 #include <unistd.h>
+/**
+* print_alphabet - Fonction qui affiche l'alphabet en minuscules.
+*
+* Description: La fonction écrit chaque lettre de 'a' à 'z' sur la sortie
+* standard, suivie d'un saut de ligne.
+*/
 void print_alphabet(void)
 {
 char letter = 'a';
@@ -10,6 +16,12 @@ letter++;
 }
 write(1, "\n", 1);
 }
+/**
+* print_alphabet_x10 - Fonction qui affiche l'alphabet en minuscules 10 fois.
+*
+* Description: La fonction appelle la fonction print_alphabet() 10 fois,
+* affichant l'alphabet à chaque itération.
+*/
 void print_alphabet_x10(void)
 {
 int i;
