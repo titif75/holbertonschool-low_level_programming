50f0fa679d2b7466c1f5e30df8656a6988e9d024
Your Name <bezom@hotmail.fr>
2025-02-19 20:39:00 +0100

    a

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+/**
+* print_sign - imprime le signe d'un nombre
+* @n: le nombre à vérifier
+*
+* Return: 1 si positif, 0 si zéro, -1 si négatif
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,5 +1,4 @@
 #ifndef MAIN_H
 #define MAIN_H
-int _putchar(char c);
-int _isalpha(int c);
+int print_sign(int n);
 #endif /* MAIN_H */
