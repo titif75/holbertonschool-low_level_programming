a5f9b1c702b91e9f814bef22b418da8a7a2939cb
Your Name <bezom@hotmail.fr>
2025-02-19 12:56:54 +0100

    a

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,36 +1,25 @@
-/* _putchar.c */
 #include <unistd.h>
-
+/* Implémentation de la fonction _putchar */
 void _putchar(char c)
 {
-    write(1, &c, 1);
+write(1, &c, 1);
 }
-/* main.h */
-#ifndef MAIN_H
-#define MAIN_H
-
-void print_alphabet(void);
-
-#endif /* MAIN_H */
-/* main.c */
-#include "main.h"
-
-int main(void)
-{
-    print_alphabet();
-    return 0;
-}
-/* 1-alphabet.c */
-#include "main.h"
-
+/* Fonction pour afficher l'alphabet en minuscules */
 void print_alphabet(void)
 {
-    char letter = 'a';
-
-    while (letter <= 'z')
-    {
-        _putchar(letter);
-        letter++;
-    }
-    _putchar('\n');
+char letter = 'a';
+/* Affiche chaque lettre de l'alphabet */
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+/* Affiche la nouvelle ligne */
+_putchar('\n');
+}
+/* Fonction main */
+int main(void)
+{
+print_alphabet();
+return 0;
 }
