7ccb2f470c48c83f97da01f5cdb39c2c2eca6b85
Your Name <bezom@hotmail.fr>
2025-02-19 20:47:51 +0100

    a

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,3 +1,4 @@
+#include "main.h"
 /**
 * print_sign - imprime le signe d'un nombre
 * @n: le nombre à vérifier
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,4 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+int _putchar(char c);
 int print_sign(int n);
+
 #endif /* MAIN_H */
