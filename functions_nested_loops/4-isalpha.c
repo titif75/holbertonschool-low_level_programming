22cf047c271721c4089d7d19d66ec4120adf8a88
Your Name <bezom@hotmail.fr>
2025-02-19 20:18:16 +0100

    a

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,4 +1,9 @@
-int _isalpha(int c)
+/**
+* _isalpha - vérifie si un caractère est alphabétique
+* @c: le caractère à vérifier
+*
+* Return: 1 si c est une lettre, 0 sinon
+*/int _isalpha(int c)
 {
 if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 return (1);
