b65bbd3bff937accd7b203c8e0325bd23856190f
Your Name <bezom@hotmail.fr>
2025-02-20 13:55:17 +0100

    a

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,31 +1,34 @@
-#include "main.h"
+#include "main.h"                                          
 /**
-* definir la table
-*/
-void times_table(void)
-{
-int i, j, product;
-for (i = 0; i < 10; i++)
-{
-for (j = 0; j < 10; j++)
-{
-product = i * j;
-if (j != 0)
-{
-_putchar(',');
-_putchar(' ');
-}
-if (product < 10 && j != 0)
-{
-_putchar(' ');
-_putchar(product + '0');
-}
-else
-{
-_putchar(product / 10 + '0');
-_putchar(product % 10 + '0');
-}
-}
-_putchar('\n');
-}
+* definir la table de 9
+*
+void times_table(void)                                      
+{                                                            
+    int i, j, product;                                      
+    for (i = 0; i < 10; i++)                                
+    {                                                        
+        for (j = 0; j < 10; j++)                            
+        {                                                    
+            product = i * j;                                
+            if (j != 0)                                     
+            {                                                
+                _putchar(',');                              
+                _putchar(' ');                              
+            }                                                
+            if (product < 10)                               
+            {                                                
+                if (j != 0)                                 
+                {                                            
+                    _putchar(' ');                           
+                }                                            
+                _putchar(product + '0');                    
+            }                                                
+            else                                            
+            {                                                
+                _putchar(product / 10 + '0');               
+                _putchar(product % 10 + '0');               
+            }                                                
+        }                                                    
+        _putchar('\n');                                      
+    }                                                        
 }
