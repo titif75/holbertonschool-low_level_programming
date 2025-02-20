0dd426e5120dcc1bd6284461f1a9ac3dbda73034
Your Name <bezom@hotmail.fr>
2025-02-20 12:59:13 +0100

    a

---
+++ functions_nested_loops/main.c
@@ -0,0 +1,9 @@
+#include "main.h"                                                          
+/**
+* main - entry program
+*/
+int main(void)                                                            
+{                                                                         
+print_to_98(105);                                                     
+return 0;                                                              
+}                                                                         
