80ab85f548b980caf9cac9c678cc66c0c02a58ab
Your Name <bezom@hotmail.fr>
2025-02-20 12:16:42 +0100

    a

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,16 +1,19 @@
-#include "main.h"  // Si vous avez un fichier d'en-tête
+#include "main.h"
+/**
+* afficher l'heure avec les heures et minutes
+*/
 void jack_bauer(void)
 {
 int h, m;
-for (h = 0; h < 24; h++)  // De 00 à 23
+for (h = 0; h < 24; h++)
 {
-for (m = 0; m < 60; m++)  // De 00 à 59
+for (m = 0; m < 60; m++)
 {
-_putchar((h / 10) + '0');  // Affiche les dizaines d'heures
-_putchar((h % 10) + '0');  // Affiche les unités d'heures
+_putchar((h / 10) + '0');
+_putchar((h % 10) + '0');
 _putchar(':');
-_putchar((m / 10) + '0');  // Affiche les dizaines de minutes
-_putchar((m % 10) + '0');  // Affiche les unités de minutes
+_putchar((m / 10) + '0');
+_putchar((m % 10) + '0');
 _putchar('\n');
 }
 }
