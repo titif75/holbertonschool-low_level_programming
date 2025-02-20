20b6991ccddea049e5f249065cdc1d6ea3d42675
Your Name <bezom@hotmail.fr>
2025-02-20 11:02:39 +0100

    a

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+int print_last_digit(int n)
+{
+    int last_digit;
+
+    /* Get absolute value of last digit using modulo */
+    last_digit = n % 10;
+    if (last_digit < 0)
+        last_digit = -last_digit;
+
+    /* Print the digit using _putchar */
+    _putchar(last_digit + '0');
+
+    return (last_digit);
+}
