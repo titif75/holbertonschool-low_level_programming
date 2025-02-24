a6bb4260664d3f82ef5ed1a78b1e94e96f1511e8
Your Name <bezom@hotmail.fr>
2025-02-24 12:47:19 +0100

    0-isupper.c

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,28 +1,8 @@
-#include <stdio.h>
 #include <main.h>
-/**
-* main - fonction majuscule
-*return : 0
-*/
-int _isupper(int c) 
+int _isupper(int c)
 {
-if (c >= 'A' && c <= 'Z') {
+if (c >= 'A' && c <= 'Z')
 return 1;  // c est une lettre majuscule
-}
+else
 return 0;  // c n'est pas une lettre majuscule
-}
-int main() {
-char c = 'A';
-if (_isupper(c)) {
-printf("%c est une lettre majuscule\n", c);
-} else {
-printf("%c n'est pas une lettre majuscule\n", c);
-}
-c = 'a';
-if (_isupper(c)) {
-printf("%c est une lettre majuscule\n", c);
-} else {
-printf("%c n'est pas une lettre majuscule\n", c);
-}
-return 0;
 } 
