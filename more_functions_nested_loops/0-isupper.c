74089f0263a9896fe74c1af47009356eb7664ad4
Your Name <bezom@hotmail.fr>
2025-02-24 12:17:24 +0100

    readme.md

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+* main - fonction majuscule
+*/
+int main(){
+char c = 'A';
+if (isupper(c)){
+printf("%c est une lettre majuscule.\n",c);
+}
+else {
+printf("%c n'est pas une lettre majuscule.\n",c;
+}
+return (0);
+}
