cc5ff888e60a11a90c89577d67d2e4067192cee7
Your Name <bezom@hotmail.fr>
2025-02-24 13:05:13 +0100

    0-isupper.c

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -3,7 +3,7 @@
 int _isupper(int c)
 {
 if (c >= 'A' && c <= 'Z')
-return 1;  // c est une lettre majuscule
+return 1; /* c est une lettre majuscule */
 else
-return 0;  // c n'est pas une lettre majuscule
+return 0; /* c n'est pas une lettre majuscule */
 } 
