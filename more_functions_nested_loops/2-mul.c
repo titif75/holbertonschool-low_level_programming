2397ab4bc6b04f0cc5ce0cbffc6989e9e484b3e2
Your Name <bezom@hotmail.fr>
2025-02-24 15:06:44 +0100

    2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* mul - multiplie deux entiers
+* @a: Le premier entier
+* @b: Le deuxième entier
+* Return: Le résultat de la multiplication de a et b
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -2,4 +2,5 @@
 #define MAIN_H
 int _putchar(char c);  /* Déclaration de la fonction _putchar */
 int _isdigit(int c); /* Déclaration de la fontion _isdigit */
+int mul(int a, int b); /* Déclaration de la fonction int mul(int a, int b) */
 #endif /* MAIN_H */
