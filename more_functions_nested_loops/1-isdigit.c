6dbc39433503757ce8cf635e7420ea78dfdb9d64
Your Name <bezom@hotmail.fr>
2025-02-24 14:46:46 +0100

    1-isdigit.c

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -4,9 +4,9 @@
 * @c: le caractere à verifier
 * Return: 1 si c est un chiffre, 0 si ce n'est pas un chiffre
 */
-int main(void)
+int _isdigit(int c);
 {
-if( c>= '0' && c<='9')
+if( c>= '0' && c<= '9')
 return (1);
 else
 return (0)
