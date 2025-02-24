cab53f4bf4957dde04a28cd3d9007492576ea1a3
Your Name <bezom@hotmail.fr>
2025-02-24 14:33:11 +0100

    1-isdigit.c

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _isdigit - la fonction qui vérifie les chiffres de 0 à 9
+* @c: le caractere à verifier
+* Return: 1 si c est un chiffre, 0 si ce n'est pas un chiffre
+*/
+int main(void)
+{
+if( c>= '0' && c<='9')
+return (1);
+else
+return (0)
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,5 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
 int _putchar(char c);  /* Déclaration de la fonction _putchar */
-int _isupper(int c);   /* Déclaration de la fonction _isupper */
+int _isdigit(int c); /* Déclaration de la fontion _isdigit */
 #endif /* MAIN_H */
