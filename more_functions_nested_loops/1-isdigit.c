10905e22a43591c45c1989cdc6850bbe021b9cc8
Your Name <bezom@hotmail.fr>
2025-02-24 14:52:45 +0100

    1-isdigit.c

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
-* _isdigit - la fonction qui vérifie les chiffres de 0 à 9
+* _isdigit - la fonction qui vérifie les chiffres de (0 à 9)
 * @c: le caractere à verifier
 * Return: 1 si c est un chiffre, 0 si ce n'est pas un chiffre
 */
 int _isdigit(int c);
 {
-if( c>= '0' && c<= '9')
+if(c>= '0' && c<= '9')
 return (1);
 else
 return (0)
