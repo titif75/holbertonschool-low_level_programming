7724f54177a04fabc50166b17e2dc15b9997fa73
Your Name <bezom@hotmail.fr>
2025-02-25 15:07:48 +0100

    7-print_diagonal.c

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,5 +1,12 @@
 #include "main.h"
-
+/**
+* print_diagonal - Dessine une ligne diagonale avec le caractère '\'.
+* @n: Le nombre de fois que le caractère '\' doit être imprimé.
+* 
+* Description: Si n est inférieur ou égal à 0, seul un saut de ligne est imprimé. 
+* Sinon, cela imprime une ligne diagonale avec le nombre spécifié de caractères '\' 
+* sur chaque ligne.
+*/
 void print_diagonal(int n)
 {
 if (n <= 0)
