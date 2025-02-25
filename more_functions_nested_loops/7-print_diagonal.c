8610dded941c34a8789d437a3ff2ff252f8f8ce5
Your Name <bezom@hotmail.fr>
2025-02-25 15:05:39 +0100

    7-print_diagonal.c

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 void print_diagonal(int n)
 {
 if (n <= 0)
@@ -7,9 +8,10 @@ _putchar('\n');
 }
 else
 {
-for (int i = 0; i < n; i++)
+int i, j;
+for (i = 0; i < n; i++)
 {
-for (int j = 0; j < i; j++)
+for (j = 0; j < i; j++)
 {
 _putchar(' ');
 }
