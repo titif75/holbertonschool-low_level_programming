f5f6c6bf723e30a32c580893029aa7a5a150abf9
Your Name <bezom@hotmail.fr>
2025-02-25 15:49:05 +0100

    8-print_square.c

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -21,14 +21,13 @@ _putchar('\n');
 }
 else
 {
-for (i = 0; i < size; i++)
+for(i = 0; i < size; i++)
 {
-for (j = 0; j < size; j++)
+for(j = 0; j < size; j++)
 {
 _putchar('#');
 }
 _putchar('\n');
-}
 }
 }
 }
