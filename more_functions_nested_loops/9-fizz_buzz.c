0fcafce392d16da354ee9da321f312bed8424b20
Your Name <bezom@hotmail.fr>
2025-02-25 16:27:10 +0100

    9-fizz_buzz.c

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+* Le programme affiche les nombres de 1 à 100, avec "Fizz"
+* pour les multiples de 3, "Buzz" pour ceux de 5, et "FizzBuzz"
+* pour ceux qui sont multiples des deux. Chaque mot ou nombre est
+* séparé par un espace, et la dernière valeur est suivie d'un saut de ligne.
+*/
+int main(void)
+{
+for (int i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+printf("FizzBuzz");
+else if (i % 3 == 0)
+printf("Fizz");
+else if (i % 5 == 0)
+printf("Buzz");
+else
+printf("%d", i);
+if (i != 100)
+printf(" ");
+else
+printf("\n");
+}
+return 0;
+}
