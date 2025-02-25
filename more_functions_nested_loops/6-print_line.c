ce50002cf7942a1b26698a4ee18569db14754b77
Your Name <bezom@hotmail.fr>
2025-02-25 13:45:02 +0100

    6-print_line.c

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* main - caractere qui imprime ligne continue
+*/
+void _putchar(char c){
+putchar(c)  
+}
+void print_line(int n){
+if (n <= 0){
+_putchar(' \n ');
+}
+else{
+for (int i = 0; i < n; i++){
+_putchar('_');
+}
+_putchar('\n);
+}
+}
+return (ø);
