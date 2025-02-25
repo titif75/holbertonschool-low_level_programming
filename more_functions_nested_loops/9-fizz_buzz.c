a2038eb85bc19b961042f1b4958355ec04c8012a
Your Name <bezom@hotmail.fr>
2025-02-25 16:46:03 +0100

    9-fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -2,9 +2,9 @@
 /**
 * main - Imprime les nombres de 1 à 100, remplaçant
 * certains par Fizz, Buzz ou FizzBuzz.
-* Cette fonction parcourt les nombres de 1 à 100 et affiche chaque nombre. 
+* Cette fonction parcourt les nombres de 1 à 100 et affiche chaque nombre.
 * Cependant, pour les multiples de 3, elle affiche "Fizz",
-* pour les multiples de 5, 
+* pour les multiples de 5,
 * elle affiche "Buzz", et pour les multiples de 3 et 5,
 * elle affiche "FizzBuzz".
 *
