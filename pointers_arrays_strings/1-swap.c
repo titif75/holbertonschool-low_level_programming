c05bc4266f618ccd7766d05dd06b27e300a53bb3
Your Name <bezom@hotmail.fr>
2025-02-26 16:20:44 +0100

    1-swap.c

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* swap_int - Échange les valeurs de deux entiers
+* @a: Pointeur vers le premier entier
+* @b: Pointeur vers le deuxième entier
+*/
+void swap_int(int *a, int *b)
+{
+int temp;
+temp = *a;
+*a = *b;
+*b = temp;
