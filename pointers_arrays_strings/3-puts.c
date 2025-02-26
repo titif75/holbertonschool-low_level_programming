19ef3db928c53652b3044ae97ec925c4ba1638fa
Your Name <bezom@hotmail.fr>
2025-02-26 16:36:28 +0100

    3-puts.c

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "main.h"
+/**
+* _puts - Imprime une chaîne de caractères suivie d'une nouvelle ligne
+* @str: Pointeur vers la chaîne de caractères
+*/
+void _puts(char *str)
+{
+while (*str)
+{
+write(1, str, 1);
+str++;
+}
+write(1,"\n", 1);
+}
