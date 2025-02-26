5a9a3376b17198c1220e565f8701a42ab546cfe5
Your Name <bezom@hotmail.fr>
2025-02-26 16:28:33 +0100

    2-strlen.c

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _strlen - Calcule la longueur d'une chaîne de caractères
+* @s: Pointeur vers la chaîne de caractères
+*
+* Return: La longueur de la chaîne
+*/
+int _strlen(char *s)
+{
+int length = 0;
+while (s[length] != '\0') 
+{
+length++;
+}
+return length;
+}
