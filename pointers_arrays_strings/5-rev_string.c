204cee7f0584a082b2bfdf2637e996567957a8bb
Your Name <bezom@hotmail.fr>
2025-02-26 16:56:53 +0100

    5-rev_string.c

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <unistd.h>
+/**
+* rev_string - Inverse une chaîne de caractères
+* @s: Pointeur vers la chaîne de caractères
+*/
+void rev_string(char *s)
+{
+int length = 0;
+int i;
+char temp;
+while (s[length] != '\0') 
+{
+length++;
+}
+for (i = 0; i < length / 2; i++) 
+{
+temp = s[i];
+s[i] = s[length - 1 - i];
+s[length - 1 - i] = temp;
+}
+}
