04fbe6646eadd9bddbd6d192969e2a19ba5985c8
Your Name <bezom@hotmail.fr>
2025-02-26 16:08:45 +0100

    0-reset_to_98.c

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,5 @@
 #include "main.h"
-/**
-* fonction qui prend un pointeur vers un int en parametre et 
-* met à jour la valeur à laquelle il pointe pour qu'elle soit egal.
-*/
-int main(){
-int num = 42;
-reset_to_98(&num);
-return (0);
+void reset_to_98(int *n)
+{
+*n = 98;
 }
