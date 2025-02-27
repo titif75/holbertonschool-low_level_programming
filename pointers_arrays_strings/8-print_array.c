ea5643ffa79f394901505887aa658372f2d95328
Your Name <bezom@hotmail.fr>
2025-02-27 16:19:05 +0100

    8-print_array.c

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,48 +1,46 @@
-#include <unistd.h>
-
-void _putchar(char c)
-{
-write(1, &c, 1);
-}
+#include "main.h"
+/**
+* print_array - prints n elements of an array of integers
+* @a: array of integers
+* @n: number of elements to print
+*
+* Return: void
+*/
 void print_array(int *a, int n)
 {
 int i;
-int num;
-int temp;
-int digits;
-int k;
-int divisor;
 for (i = 0; i < n; i++)
 {
-num = a[i];
-if (num == 0)
-{
-_putchar('0');
-}
-else
-{
-if (num < 0)
-{
-_putchar('-');
-num = -num;
-}
-int divisor = 1;
-while (num / divisor >= 10)
-{
-divisor *= 10;
-}
-while (divisor > 0)
-{
-_putchar((num / divisor) + '0');
-num %= divisor;
-divisor /= 10;
-}
-}
-if (i < n - 1) 
+if (i != 0)
 {
 _putchar(',');
 _putchar(' ');
 }
+print_number(a[i]);
 }
 _putchar('\n');
+}
+/**
+* print_number - prints an integer using _putchar
+* @n: integer to print
+*
+* Return: void
+*/
+void print_number(int n)
+{
+unsigned int num;
+/* Handle negative numbers */
+if (n < 0)
+{
+_putchar('-');
+num = -n;
+}
+else
+{
+num = n;
+}
+/* Print recursively */
+if (num / 10)
+print_number(num / 10);
+_putchar((num % 10) + '0');
 }
