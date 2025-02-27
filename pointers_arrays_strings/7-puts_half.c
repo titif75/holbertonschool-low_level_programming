31d7f22b3c52ee0cc111e2e91ee63588a9f6eb09
Your Name <bezom@hotmail.fr>
2025-02-27 13:48:08 +0100

    7-puts_half.c

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,22 +1,27 @@
 #include <unistd.h>
 
 /**
-* puts_half - Affiche la seconde moitié d'une chaîne, suivie d'un saut de ligne.
+* puts_half - Affiche la seconde moitié d'une chaîne,
+* suivie d'un saut de ligne.
 * @str: Pointeur vers la chaîne de caractères
 */
 void puts_half(char *str)
 {
 int length = 0;
 int i;
-while (str[length] != '\0') {
+while (str[length] != '\0') 
+{
 length++;
 }
-if (length % 2 == 0) {
+if (length % 2 == 0) 
+{
 i = length / 2;
-} else {
+} else 
+{
 i = (length + 1) / 2;
 }
-while (str[i] != '\0') {
+while (str[i] != '\0') 
+{
 write(1, &str[i], 1);
 i++;
 }
