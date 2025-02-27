11a18ad42fd0b28b27867ca931ddf96814c86686
Your Name <bezom@hotmail.fr>
2025-02-27 16:40:58 +0100

    9-strcpy.c

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main"
+/**
+* _strcpy - copies the string pointed to by src to dest
+* @dest: pointer to the destination buffer
+* @src: pointer to the source string
+*
+* Return: pointer to dest
+*/
+char *_strcpy(char *dest, char *src)
+{
+int i = 0;
+/* Copy characters from src to dest until null terminator */
+while (src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+/* Copy the null terminator */
+dest[i] = '\0';    
+/* Return pointer to destination */
+return dest;
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -7,6 +7,6 @@ void rev_string(char *s);
 void print_rev(char *s);
 int _strlen(char *s);
 void swap_int(int *a, int *b);
-
+char *_strcpy(char *dest, char *src);
 void reset_to_98(int *n);
 #endif /* MAIN_H */
