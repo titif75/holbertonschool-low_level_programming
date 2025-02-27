ee83c6e707d01546913bbbdf8b95c9e9d95f2521
Your Name <bezom@hotmail.fr>
2025-02-27 16:27:33 +0100

    8-print_array.c

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,9 +1,8 @@
-#include "main.h"
+#include <stdio.h>
 /**
 * print_array - prints n elements of an array of integers
 * @a: array of integers
 * @n: number of elements to print
-*
 * Return: void
 */
 void print_array(int *a, int n)
@@ -13,34 +12,9 @@ for (i = 0; i < n; i++)
 {
 if (i != 0)
 {
-_putchar(',');
-_putchar(' ');
+printf(", ");
 }
-print_number(a[i]);
+printf("%d", a[i]);
 }
-_putchar('\n');
-}
-/**
-* print_number - prints an integer using _putchar
-* @n: integer to print
-*
-* Return: void
-*/
-void print_number(int n)
-{
-unsigned int num;
-/* Handle negative numbers */
-if (n < 0)
-{
-_putchar('-');
-num = -n;
-}
-else
-{
-num = n;
-}
-/* Print recursively */
-if (num / 10)
-print_number(num / 10);
-_putchar((num % 10) + '0');
+printf("\n");
 }
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -7,5 +7,6 @@ void rev_string(char *s);
 void print_rev(char *s);
 int _strlen(char *s);
 void swap_int(int *a, int *b);
+
 void reset_to_98(int *n);
 #endif /* MAIN_H */
