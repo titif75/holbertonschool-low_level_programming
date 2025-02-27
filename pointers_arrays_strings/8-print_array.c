43dbfc301d524429c12a3e04a1d74286c28f42e9
Your Name <bezom@hotmail.fr>
2025-02-27 15:22:59 +0100

    8-print_array.c

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,40 +1,41 @@
 #include <unistd.h>
-void _putchar(char c) {
+
+void _putchar(char c)
+{
 write(1, &c, 1);
 }
-void print_array(int *a, int n) {
+void print_array(int *a, int n)
+{
 int i;
 int num;
-char str[10];
-int temp;
-int digits;
-int k;
-for (i = 0; i < n; i++) {
-num = a[i];
-if (num == 0) {
-_putchar('0');
-} 
-else 
+for (i = 0; i < n; i++)
 {
-if (num < 0) {
+num = a[i];
+if (num == 0)
+{
+_putchar('0');
+}
+else
+{
+if (num < 0)
+{
 _putchar('-');
 num = -num;
 }
-temp = num;
-digits = 0;
-while (temp > 0) {
-temp /= 10;
-digits++;
+int divisor = 1;
+while (num / divisor >= 10)
+{
+divisor *= 10;
 }
-for (k = digits - 1; k >= 0; k--) {
-str[k] = (num % 10) + '0';
-num /= 10;
-}
-for (k = 0; k < digits; k++) {
-_putchar(str[k]);
+while (divisor > 0)
+{
+_putchar((num / divisor) + '0');
+num %= divisor;
+divisor /= 10;
 }
 }
-if (i < n - 1) {
+if (i < n - 1) 
+{
 _putchar(',');
 _putchar(' ');
 }
