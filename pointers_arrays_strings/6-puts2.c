6223e3d41a5c90a7db72674453e007ff5e1308b6
Your Name <bezom@hotmail.fr>
2025-02-27 13:16:43 +0100

    6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+/**
+* puts2 - Affiche chaque autre caractère d'une chaîne,
+* en commençant par le premier.
+* @str: Pointeur vers la chaîne de caractères
+*/
+void puts2(char *str)
+{
+int i = 0;
+while (str[i] != '\0') 
+{
+if (i % 2 == 0) 
+{
+write(1, &str[i], 1);
+}
+i++;
+}
+write(1, "\n", 1);
+}
