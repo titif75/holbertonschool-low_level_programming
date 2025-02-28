f87e9b8840e5b5731ab51374236fb79ffe099d6f
Your Name <bezom@hotmail.fr>
2025-02-28 15:00:00 +0100

    7-leet.c

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -6,13 +6,13 @@
 */
 char *leet(char *s)
 {
-int i, j;
+int i;
+int j;
 char replacements[] = "aAeEoOtTlL";
 char codes[] = "4433007711";
 for (i = 0; s[i] != '\0'; i++)
 {
-for (int j = 0; replacements[j] != '\0'; j++)
-{
+for (j = 0; replacements[j] != '\0'; j++)
 if (s[i] == replacements[j])
 {
 s[i] = codes[j];
