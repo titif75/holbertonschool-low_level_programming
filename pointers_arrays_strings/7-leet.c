088344ffc2e14c106183ee3ac2e7c27b12e8615a
Your Name <bezom@hotmail.fr>
2025-02-28 14:43:09 +0100

    8-print_array.c

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* leet - Encodes a string into 1337.
+* @s: The string to be encoded.
+* Return: The encoded string.
+*/
+char *leet(char *s)
+{
+int i;
+char replacements[] = "aAeEoOtTlL";
+char codes[] = "4433007711";
+for (i = 0; s[i] != '\0'; i++)
+{
+for (int j = 0; replacements[j] != '\0'; j++)
+{
+if (s[i] == replacements[j])
+{
+s[i] = codes[j];
+break;
+}
+}
+}
+return s;
+}
