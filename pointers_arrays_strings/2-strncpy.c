4b277f74b7f4013197a74cb2f131c559d266c479
Your Name <bezom@hotmail.fr>
2025-02-28 13:32:28 +0100

    2-str

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+* _strncat - concatenates two strings using at most n bytes from src
+* @dest: destination string
+* @src: source string
+* @n: maximum number of bytes to use from src
+*
+* Return: pointer to the resulting string dest
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+int dest_len = 0;
+int i = 0;
+/* Find the end of dest string */
+while (dest[dest_len] != '\0')
+{
+dest_len++;
+}
+/* Append at most n bytes from src to dest */
+while (i < n && src[i] != '\0')
+{
+dest[dest_len + i] = src[i];
+i++;
+}
+/* Add terminating null byte */
+dest[dest_len + i] = '\0';
+return (dest);
+}
