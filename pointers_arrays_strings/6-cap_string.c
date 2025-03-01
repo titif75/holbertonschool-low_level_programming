bf6cb8cac90812fb4bd51ce7d1c54edc6bcc113c
Your Name <bezom@hotmail.fr>
2025-03-01 07:23:26 +0100

    6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,25 +1,34 @@
 #include "main.h"
-char *cap_string(char *str)
+char *cap_string(char *str) 
 {
-bool new_word = true;
-for (int i = 0; str[i] != '\0'; i++)
+int new_word = 1;
+int i;
+for (i = 0; str[i] != '\0'; i++)
 {
-if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == ',' || 
-str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?' || 
-str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || 
-str[i] == '}') 
+if (str[i] == ' ' || str[i] == '\t' || 
+str[i] == '\n' || str[i] == ',' || 
+str[i] == ';' || str[i] == '.' || 
+str[i] == '!' || str[i] == '?' || 
+str[i] == '"' || str[i] == '(' || 
+str[i] == ')' || str[i] == '{' || 
+str[i] == '}')
 {
-new_word = true;
+new_word = 1;
 }
 else if (new_word && str[i] >= 'a' && str[i] <= 'z')
 {
 str[i] = str[i] - 'a' + 'A';
-new_word = false;
+new_word = 0;
 } 
 else 
 {
-new_word = false;
+new_word = 0;
 }
 }
 return str;
+}
+int main() {
+char str[] = "hello, world! this is a test.";
+cap_string(str);
+return 0;
 }
