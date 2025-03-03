34627f58636acf1b3a18f1878eeee5ed0191cb35
Your Name <bezom@hotmail.fr>
2025-03-03 13:03:31 +0100

    2-strchr.c

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -21,4 +21,5 @@ char *cap_string(char *);
 char *string_toupper(char *);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+2-strchr.c
 #endif /* MAIN_H */
