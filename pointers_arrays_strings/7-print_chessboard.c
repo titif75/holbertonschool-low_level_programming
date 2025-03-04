9d0f7c19fe2e974c4701e938e77f3e302461e63a
Your Name <bezom@hotmail.fr>
2025-03-04 10:40:49 +0100

    7-print_chessboard.c

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,5 +1,6 @@
 #include "main.h"
-/** affichage d'un echiquier de 8x8 dans le terminal,
+/**
+* affichage d'un echiquier de 8x8 dans le terminal,
 * tableau 2D
 */
 void print_chessboard(char (*a)[8])
@@ -7,7 +8,7 @@ void print_chessboard(char (*a)[8])
 int i, j;
 for (i = 0; i < 8; i++)
 {
-for (j = 0; j < 8; j++) 
+for (j = 0; j < 8; j++)
 {
 write(1, &a[i][j], 1);
 }
