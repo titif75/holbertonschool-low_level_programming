edfb9e5c6be6b50550337f14c1d3b05e47d5aff0
Your Name <bezom@hotmail.fr>
2025-03-05 13:48:49 +0100

    1-memcpy.c

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,18 +1,19 @@
 #include "main.h"
 /**
-* _memcpy - Copies n bytes from memory area src to memory area dest
-* @dest: Pointer to the destination memory area
-* @src: Pointer to the source memory area
-* @n: Number of bytes to copy
+* _memset - Fills the first n bytes of the memory area pointed
+* to by s with the constant byte b.
+* s: Pointer to the memory area to fill.
+* b: The constant byte to fill the memory area with.
+* n: The number of bytes to be filled.
 *
-* Return: Pointer to dest
+* Return: A pointer to the memory area s.
 */
-char *_memcpy(char *dest, char *src, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 unsigned int i;
 for (i = 0; i < n; i++)
 {
-dest[i] = src[i];
+s[i] = b;
 }
-return (dest);
+return (s);
 }
