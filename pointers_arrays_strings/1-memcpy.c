f0769d7051b28fcb48bdbf517be8337bb635543e
Your Name <bezom@hotmail.fr>
2025-03-05 13:59:34 +0100

    1-memcpy.c

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -8,10 +8,10 @@
 *
 * Return: A pointer to the memory area s.
 */
-char *_memset(char *s, char b, unsigned int n)
+char *_memset(char *s, char b, unsigned int n);
 {
 unsigned int i;
-for (i = 0; i < n; i++)
+for (i = 0; i < n; i++);
 {
 s[i] = b;
 }
