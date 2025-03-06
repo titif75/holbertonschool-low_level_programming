50356b172fd5498a46669f9e110db6cd7d375ca4
Your Name <bezom@hotmail.fr>
2025-03-06 11:40:59 +0100

    3-strspn.c

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -4,28 +4,27 @@
 * @s: The string to be scanned
 * @accept: The string containing the characters to match
 *
-* Return: Number of bytes in initial segment of s which consist only
-* of bytes from accept
+* Return: The number of bytes in the initial segment of `s`
+*         which consist only of bytes from `accept`
 */
 unsigned int _strspn(char *s, char *accept)
 {
 unsigned int count = 0;
-int i, j;
-int found;
-for (i = 0; s[i] != '\0'; i++)
+int i, found;
+while (*s)
 {
 found = 0;
-for (j = 0; accept[j] != '\0'; j++)
+for (i = 0; accept[i]; i++)
 {
-if (s[i] == accept[j])
+if (*s == accept[i])
 {
 found = 1;
 break;
 }
 }
-if (found == 0)
-return (count);
+if (!found)
+break;
 count++;
+s++;
 }
-return (count);
-}
+return (count);}
