01aca2ec4647c103e400e271e2dd99aba667b98e
Your Name <bezom@hotmail.fr>
2025-03-06 13:53:41 +0100

    6-is_prime_number.c

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -7,8 +7,6 @@
 */
 int is_prime_number(int n)
 {
-if (n <= 1)
-return (0);
 return (check_prime(n, 2));
 }
 /**
@@ -20,7 +18,9 @@ return (check_prime(n, 2));
 */
 int check_prime(int n, int i)
 {
-if (i * i > n)
+if (n <= 1)
+return (0);
+if (n == i)
 return (1);
 if (n % i == 0)
 return (0);
