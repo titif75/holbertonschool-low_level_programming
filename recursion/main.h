194f2e7e22cb45c59b61dfee30cb066a53a7aec7
Your Name <bezom@hotmail.fr>
2025-03-06 13:29:11 +0100

    4-pow_recursion.c

--- recursion/main.h
+++ recursion/main.h
@@ -5,4 +5,6 @@ void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
+#include "main.h"
+int _pow_recursion(int x, int y);
 #endif /* MAIN_H */
