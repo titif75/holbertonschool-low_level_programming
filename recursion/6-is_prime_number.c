61f0e924a2534c80ac3410b450f8f84be5f68b96
Your Name <bezom@hotmail.fr>
2025-03-06 14:00:19 +0100

    6-is_prime_number.c

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -7,22 +7,14 @@
 */
 int is_prime_number(int n)
 {
-return (check_prime(n, 2));
-}
-/**
-* check_prime - recursive helper function to check primality
-* @n: number to check
-* @i: current divisor to check
-*
-* Return: 1 if prime, 0 if not
-*/
-int check_prime(int n, int i)
-{
+int i, sqrt;
 if (n <= 1)
 return (0);
-if (n == i)
-return (1);
+sqrt = _sqrt_recursion(n);
+for (i = 2; i <= sqrt; i++)
+{
 if (n % i == 0)
 return (0);
-return (check_prime(n, i + 1));
+}
+return (1);
 }
