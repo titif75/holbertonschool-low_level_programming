0ca3c38c232083b15121245112e418808060e3f3
Your Name <bezom@hotmail.fr>
2025-03-06 11:56:49 +0100

    7-print_chessboard.c

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,25 +1,17 @@
 #include "main.h"
 /**
-* @brief Affiche un échiquier 8x8 dans la sortie standard.
-*
-* Cette fonction parcourt un tableau 2D de 8x8 caractères
-* représentant un échiquier
-* et affiche chaque case sur l'écran. Les caractères dans
-* le tableau représentent des
-* pièces d'échiquier (par exemple, 'R' pour une tour
-* blanche, 'r' pour une tour noire).
-*
-* @param a Un tableau 2D de 8x8 caractères représentant l'échiquier.
+* print_chessboard - Prints the chessboard
+* @a: 2D array representing the chessboard
 */
-void print_chessboard(char (*a)[8]);
+void print_chessboard(char (*a)[8])
 {
 int i, j;
 for (i = 0; i < 8; i++)
 {
 for (j = 0; j < 8; j++)
 {
-write(1, &a[i][j], 1);
+_putchar(a[i][j]);
 }
-write(1, "\n", 1);
+_putchar('\n');
 }
 }
