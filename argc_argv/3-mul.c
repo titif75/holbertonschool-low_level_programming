7693bd4fa4a30f5b5dec4024bc45b017e8dd963e
Your Name <bezom@hotmail.fr>
2025-03-07 05:32:11 +0100

    3-mul.c

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* main - multiplie deux nombres
+* @argc: nombre d'arguments
+* @argv: tableau des arguments
+* Return: 0 si succès, 1 si erreur
+*/
+int main(int argc, char *argv[])
+{
+int num1, num2, result;
+if (argc != 3)
+{
+_putchar('E');
+_putchar('r');
+_putchar('r');
+_putchar('o');
+_putchar('r');
+_putchar('\n');
+return (1);
+}
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+result = num1 * num2;
+print_number(result);
+_putchar('\n');
+return (0);
+}
--- argc_argv/main.h
+++ argc_argv/main.h
@@ -2,4 +2,6 @@
 #define MAIN_H
 int _putchar(char c);
 void print_program_name(char *name);
+int _atoi(char *s);
+void print_number(int n);
 #endif
