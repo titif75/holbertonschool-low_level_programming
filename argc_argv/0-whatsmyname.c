51dc151e307c9862a87d263a35fa8abeda115826
Your Name <bezom@hotmail.fr>
2025-03-07 04:51:41 +0100

    0-whatsmyname.c

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* @argc: Nombre d'arguments
+* @argv: Tableau des arguments
+* Return: 0 en cas de succès
+*/
+int main(int argc, char *argv[])
+{
+char *prog_name = argv[0];
+int i = 0;
+while (prog_name[i])
+{
+_putchar(prog_name[i]);
+i++;
+}
+_putchar('\n');
+return (0);
+}
