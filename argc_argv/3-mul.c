76a3c6b3355b044dde0c191e9771f475190f5a99
Your Name <bezom@hotmail.fr>
2025-03-07 07:16:27 +0100

    3-mul.c

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,14 +1,19 @@
 #include "main.h"
-/*
-* Programme qui multiplie deux nombres entiers passés
-* en arguments.Si le nombre d'arguments n'est pas valide,
-* le programme retourne 1.Sinon, il effectue la multiplication
-* et termine sans afficher le résultat.
+/**
+* main - Multiplie deux nombres et affiche le résultat
+* @argc: Nombre d'arguments
+* @argv: Tableau des arguments
+*
+* Si le programme reçoit deux arguments, il multiplie 
+* les deux et affiche le résultat.
+* Si le nombre d'arguments est incorrect, il affiche
+* "Error" et retourne 1.
 */
 int main(int argc, char *argv[])
 {
-if (argc != 3)
+if (argc != 3) 
 {
+print_program_name("Error\n");
 return (1);
 }
 int num1 = 0, num2 = 0;
@@ -16,10 +21,38 @@ for (int i = 0; argv[1][i] != '\0'; i++)
 {
 num1 = num1 * 10 + (argv[1][i] - '0');
 }
-for (int i = 0; argv[2][i] != '\0'; i++) 
+for (int i = 0; argv[2][i] != '\0'; i++)
 {
 num2 = num2 * 10 + (argv[2][i] - '0');
 }
-int result = num1 *num2;
+int result = num1 * num2;
+print_number(result);
+_putchar('\n');
 return (0);
+}
+int _putchar(char c)
+{
+write(1, &c, 1);
+return (1);
+}
+void print_program_name(char *name)
+{
+while (*name)
+{
+_putchar(*name);
+name++;
+}
+}
+void print_number(int n)
+{
+if (n < 0)
+{
+_putchar('-');
+n = -n;
+}
+if (n / 10 != 0)
+{
+print_number(n / 10);
+}
+_putchar(n % 10 + '0');
 }
