e71b7d963615bfcefc63baee08ac40ea4a4c0a2a
Your Name <bezom@hotmail.fr>
2025-04-03 14:16:44 +0200

    0-print_dlistint.c

--- singly_linked_lists/doubly_linked_lists/0-print_dlistint.c
+++ singly_linked_lists/doubly_linked_lists/0-print_dlistint.c
@@ -1,4 +1,7 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 /**
  * print_dlistint - Prints all the elements of a dlistint_t list.
  * @h: Pointer to the head of the list.
@@ -7,14 +10,12 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t count = 0;
-
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        count++;
-    }
-
-    return (count);
+size_t count = 0;
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+count++;
+}
+return (count);
 }
